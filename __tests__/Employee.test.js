6abf70b85aba471168e5ebdf9360f5b97e956eb0
box-o-water <boxooowater@gmail.com>
2023-01-16 20:05:10 -0600

    test(Employee): make tests more granular

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,30 +8,51 @@ describe("Employee object", () => {
 });
 
 describe("Employee parameters", () => {
-    it("should create an employee with specific parameters", () => {
+    it("should create an employee with name Sue", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.name).toBe("Sue");
+    });
+});
+
+describe("Employee parameters", () => {
+    it("should create an employee with id 1", () => {
+    const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.id).toBe(1);
+    });
+});
+
+describe("Employee parameters", () => {
+    it("should create an employee with email sue@work.com", () => {
+    const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.email).toBe("sue@work.com");
     });
 });
 
 describe("Employee getters", () => {
-    it("should retrieve specific values via getters", () => {
+    it("should retrieve the name Sue with getName", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getName()).toBe("Sue");
-    expect(result.getId()).toBe(1);
-    expect(result.getEmail()).toBe("sue@work.com");
-    expect(result.getRole()).toBe("Employee");
     });
 });
 
 describe("Employee getters", () => {
-    it("should retrieve values via getters", () => {
+    it("should retrieve the id 1 with getID", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getName()).toBe("Sue");
     expect(result.getId()).toBe(1);
+    });
+});
+
+describe("Employee getters", () => {
+    it("should retrieve the email sue@work.com with getEmail", () => {
+    const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getEmail()).toBe("sue@work.com");
+    });
+});
+
+describe("Employee getters", () => {
+    it("should retrieve the role Employee with getRole", () => {
+    const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getRole()).toBe("Employee");
     });
 });
