a2d897dfce12e6eeb601d99878ad9090c5e00c9a
box-o-water <boxooowater@gmail.com>
2023-01-16 20:33:34 -0600

    test(Emp-and-Eng): reorganize based on data value

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,56 +7,38 @@ describe("Employee object", () => {
     });
 });
 
-describe("Employee parameters", () => {
-    it("should create an employee with name Sue", () => {
+describe("name", () => {
+    it("should return the correct name", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.name).toBe("Sue");
     expect(result.name).not.toBe("Bob");
-    });
-});
-
-describe("Employee parameters", () => {
-    it("should create an employee with id 1", () => {
-    const result = new Employee("Sue", 1, "sue@work.com");
-    expect(result.id).toBe(1);
-    expect(result.id).not.toBe(2);
-    });
-});
-
-describe("Employee parameters", () => {
-    it("should create an employee with email sue@work.com", () => {
-    const result = new Employee("Sue", 1, "sue@work.com");
-    expect(result.email).toBe("sue@work.com");
-    expect(result.email).not.toBe("sue@home.com");
-    });
-});
-
-describe("Employee getters", () => {
-    it("should retrieve the name Sue with getName", () => {
-    const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getName()).toBe("Sue");
     expect(result.getName()).not.toBe("Bob");
     });
 });
 
-describe("Employee getters", () => {
-    it("should retrieve the id 1 with getID", () => {
+describe("id", () => {
+    it("should return the correct id", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
+    expect(result.id).toBe(1);
+    expect(result.id).not.toBe(2);
     expect(result.getId()).toBe(1);
     expect(result.getId()).not.toBe(2);
     });
 });
 
-describe("Employee getters", () => {
-    it("should retrieve the email sue@work.com with getEmail", () => {
+describe("email", () => {
+    it("should return the correct email", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
+    expect(result.email).toBe("sue@work.com");
+    expect(result.email).not.toBe("sue@home.com");
     expect(result.getEmail()).toBe("sue@work.com");
     expect(result.getEmail()).not.toBe("sue@home.com");
     });
 });
 
-describe("Employee getters", () => {
-    it("should retrieve the role Employee with getRole", () => {
+describe("role", () => {
+    it("should return the correct role", () => {
     const result = new Employee("Sue", 1, "sue@work.com");
     expect(result.getRole()).toBe("Employee");
     expect(result.getRole()).not.toBe("Engineer");
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -7,64 +7,48 @@ describe("Engineer object", () => {
     });
 });
 
-describe("Engineer parameters", () => {
-    it("should create an engineer with name Sue", () => {
+describe("name", () => {
+    it("should return the correct name", () => {
     const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
     expect(result.name).toBe("Sue");
     expect(result.name).not.toBe("Bob");
-    });
-});
-
-describe("Engineer parameters", () => {
-    it("should create an engineer with id 1", () => {
-    const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
-    expect(result.id).toBe(1);
-    expect(result.id).not.toBe(2);
-    });
-});
-
-describe("Engineer parameters", () => {
-    it("should create an engineer with email sue@work.com", () => {
-    const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
-    expect(result.email).toBe("sue@work.com");
-    expect(result.email).not.toBe("sue@home.com");
-    });
-});
-
-describe("Engineer getters", () => {
-    it("should retrieve the name Sue with getName", () => {
-    const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
     expect(result.getName()).toBe("Sue");
     expect(result.getName()).not.toBe("Bob");
     });
 });
 
-describe("Engineer getters", () => {
-    it("should retrieve the id 1 with getID", () => {
+describe("id", () => {
+    it("should return the correct id", () => {
     const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
+    expect(result.id).toBe(1);
+    expect(result.id).not.toBe(2);
     expect(result.getId()).toBe(1);
     expect(result.getId()).not.toBe(2);
     });
 });
 
-describe("Engineer getters", () => {
-    it("should retrieve the email sue@work.com with getEmail", () => {
+describe("email", () => {
+    it("should return the correct email", () => {
     const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
+    expect(result.email).toBe("sue@work.com");
+    expect(result.email).not.toBe("sue@home.com");
     expect(result.getEmail()).toBe("sue@work.com");
     expect(result.getEmail()).not.toBe("sue@home.com");
     });
 });
 
-describe("Engineer getters", () => {
-    it("should retrieve the email sue@work.com with getGithub", () => {
+describe("github", () => {
+    it("should return the correct github", () => {
     const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
+    expect(result.github).toBe("github.com/sue");
+    expect(result.github).not.toBe("github.com/bob");
     expect(result.getGithub()).toBe("github.com/sue");
     expect(result.getGithub()).not.toBe("github.com/bob");
     });
 });
 
-describe("Engineer getters", () => {
-    it("should retrieve the role Engineer with getRole", () => {
+describe("role", () => {
+    it("should return the correct role", () => {
     const result = new Engineer("Sue", 1, "sue@work.com", "github.com/sue");
     expect(result.getRole()).toBe("Engineer");
     expect(result.getRole()).not.toBe("Employee");
