621f77603a381ecedd19f9fb5d28ca807b555a90
box-o-water <boxooowater@gmail.com>
2023-01-20 19:02:10 -0600

    style(style-and-formatting): added bulma styling, cleaned up formatting via prettier

--- htmlwriter.js
+++ htmlwriter.js
@@ -1,89 +1,108 @@
 const yippie = [];
 
 // Control the employee order in the array by pushing the manager first, then the engineers, then the interns.
-const populateYippie = blah => {
-    // Push employees...
-    yippie.push(blah
-        // ...that are Managers...
-        .filter(seriously => seriously.getRole() === "Manager")
-        // ...to a new array, using .map, calling a function to create a div for each
-        .map(penguin => divManager(penguin))
-    );
+const populateYippie = (blah) => {
+  // Push employees...
+  yippie.push(
+    blah
+      // ...that are Managers...
+      .filter((seriously) => seriously.getRole() === "Manager")
+      // ...to a new array, using .map, calling a function to create a div for each
+      .map((penguin) => divManager(penguin))
+  );
 
-    // Push employees...    
-    yippie.push(blah
-        // ...that are Engineers...
-        .filter(seriously => seriously.getRole() === "Engineer")
-        // ...to a new array, using .map, calling a function to create a div for each
-        .map(penguin => divEngineer(penguin))
-    );
+  // Push employees...
+  yippie.push(
+    blah
+      // ...that are Engineers...
+      .filter((seriously) => seriously.getRole() === "Engineer")
+      // ...to a new array, using .map, calling a function to create a div for each
+      .map((penguin) => divEngineer(penguin))
+  );
 
-    // Push employees...   
-    yippie.push(blah
-        // ...that are Interns...
-        .filter(seriously => seriously.getRole() === "Intern")
-        // ...and use .map to create a new array, calling a function on every intern
-        .map(penguin => divIntern(penguin))
-    );
-    
-    // join the three arrays from above into a single string object with no spaces
-    // as html ignores spacing between elements
-    console.log("final:", yippie);
-    return yippie.join("");  
+  // Push employees...
+  yippie.push(
+    blah
+      // ...that are Interns...
+      .filter((seriously) => seriously.getRole() === "Intern")
+      // ...and use .map to create a new array, calling a function on every intern
+      .map((penguin) => divIntern(penguin))
+  );
+
+  // join the three arrays from above into a single string object with no spaces
+  // as html ignores spacing between elements
+  console.log("final:", yippie);
+  // remove comma's in the arrays when more than one of the same employee type is created
+  return yippie.join("").replace(">,<", "><");
 };
 
 // Create div for Manager.
-const divManager = penguin => {
-    return `<div id="manager">
-    <h2>${penguin.getName()}</h2>
-    <ul>
-        <li>Role: ${penguin.getRole()}</li>
-        <li>Id: ${penguin.getId()}</li>
-        <li>Email: <a href="mailto:${penguin.getEmail()}">${penguin.getEmail()}</a></li>
-        <li>Office Number: ${penguin.getOfficeNumber()}</li>
-    </ul>
-</div>`
+const divManager = (penguin) => {
+  return `<div class="column is-4-tablet is-3-desktop">
+  <div class="card has-background-grey-dark has-text-white-ter" id="manager">
+    <h2 class="is-size-4 has-text-weight-bold ml-2">${penguin.getName()}</h2>
+    <h3 class="is-size-5 has-text-weight-semibold ml-2">${penguin.getRole()}</h3>
+    <div class="card-content has-background-link-light has-text-grey-dark">
+      <p>Id: ${penguin.getId()}</p>
+      <p>Email: <a href="mailto:${penguin.email}">${penguin.email}</a></p>
+      <p>Office Number: ${penguin.getOfficeNumber()}</p>
+    </div>
+  </div>
+</div>`;
 };
 
-const divEngineer = penguin => {
-    return `<div id="engineer">
-    <h2>${penguin.getName()}</h2>
-    <ul>
-        <li>Role: ${penguin.getRole()}</li>
-        <li>Id: ${penguin.getId()}</li>
-        <li>Email: <a href="mailto:${penguin.getEmail()}">${penguin.getEmail()}</a></li>
-        <li>Github: ${penguin.getGithub()}</li>
-    </ul>
-</div>
-`
+// Create div for Engineers.
+const divEngineer = (penguin) => {
+  return `<div class="column is-4-tablet is-3-desktop">
+  <div class="card has-background-grey-dark has-text-white-ter" id="engineer">
+    <h2 class="is-size-4 has-text-weight-bold ml-2">${penguin.getName()}</h2>
+    <h3 class="is-size-5 has-text-weight-semibold ml-2">${penguin.getRole()}</h3>
+    <div class="card-content has-background-success-light has-text-grey-dark">
+      <p>Id: ${penguin.getId()}</p>
+      <p>Email: <a href="mailto:${penguin.email}">${penguin.email}</a></p>
+      <p>Github: <a href="https://github.com/${penguin.getGithub()}">${penguin
+    .getGithub()
+    .replace("https://github.com/", "")}</a></p>
+    </div>
+  </div>
+</div>`;
 };
 
-const divIntern = penguin => {
-    return `<div id="intern">
-    <h2>${penguin.getName()}</h2>
-    <ul>
-        <li>Role: ${penguin.getRole()}</li>
-        <li>Id: ${penguin.getId()}</li>
-        <li>Email: <a href="mailto:${penguin.getEmail()}">${penguin.getEmail()}</a></li>
-        <li>School: ${penguin.getSchool()}</li>
-    </ul>
-</div>
-`
+// Create div for Interns.
+const divIntern = (penguin) => {
+  return `<div class="column is-4-tablet is-3-desktop">
+  <div class="card has-background-grey-dark has-text-white-ter" id="intern">
+    <h2 class="is-size-4 has-text-weight-bold ml-2">${penguin.getName()}</h2>
+    <h3 class="is-size-5 has-text-weight-semibold ml-2">${penguin.getRole()}</h3>
+    <div class="card-content has-background-warning-light has-text-grey-dark">
+      <p>Id: ${penguin.getId()}</p>
+      <p>Email: <a href="mailto:${penguin.getEmail()}">${penguin.getEmail()}</a></p>
+      <p>School: ${penguin.getSchool()}</p>
+    </div>
+  </div>
+</div>`;
 };
 
-const andTheRest = penguin =>{
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-    ${populateYippie(penguin)}
-    </body>
-    </html>`
-}
+// Create the top level elements, and nest the employee divs within the body.
+const andTheRest = (penguin) => {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+  <title>My Team</title>
+</head>
+<body> 
+  <div class="container">
+    <h1 class="title has-text-centered has-text-grey-dark has-text-weight-bold is-size-2 mt-1">My Team</h1>
+    <div class="columns is-multiline is-variable is-centered">
+${populateYippie(penguin)}
+    </div>
+  </div>
+</body>
+</html>`;
+};
 
 module.exports = andTheRest;
--- index.js
+++ index.js
@@ -1,178 +1,197 @@
 // Load modules
-const Manager = require("./lib/Manager")
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const makeThing = require('./htmlwriter');
+const makeThing = require("./htmlwriter");
 
 let employees = [];
 
 function manager() {
-    console.log("a manager");
+  console.log("a manager");
 
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Name:",
-            name: "name",
-            default: "Sue" // remove when ready
-        },
-        {
-            type: "input",
-            message: "Employee ID:",
-            name: "id",
-            default: 1 // remove when ready
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "email",
-            default: "sue@work.com" // remove when ready
-        },
-        {
-            type: "input",
-            message: "Office number:",
-            name: "office",
-            default: "1a" // remove when ready
-        },
+      {
+        type: "input",
+        message: "Name:",
+        name: "name",
+        default: "Jen Barber",
+      },
+      {
+        type: "input",
+        message: "Employee ID:",
+        name: "id",
+        default: 5,
+      },
+      {
+        type: "input",
+        message: "Email address:",
+        name: "email",
+        default: "j.b@reynholm.com",
+      },
+      {
+        type: "input",
+        message: "Office number:",
+        name: "office",
+        default: "B",
+      },
     ])
     .then(function (userInput) {
-        let manager = new Manager(userInput.name, userInput.id, userInput.email, userInput.office);
-        employees.push(manager)
-        console.log(employees);
-        team()
+      let manager = new Manager(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.office
+      );
+      employees.push(manager);
+      console.log(employees);
+      team();
     });
 }
 
 function team() {
-
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "list",
-            message: "Select employee type:",
-            name: "empType",
-            choices: ["Engineer", "Intern"],
-            default: "Engineer"
-        },
+      {
+        type: "list",
+        message: "Select employee type:",
+        name: "empType",
+        choices: ["Engineer", "Intern"],
+        default: "Engineer",
+      },
     ])
     .then(function (userInput) {
-        switch(userInput.empType) {
-            case "Manager":
-                manager();
-                break;
-            case "Engineer":
-                engineer();
-                break;
-            default:
-                intern();
-        }
+      switch (userInput.empType) {
+        case "Manager":
+          manager();
+          break;
+        case "Engineer":
+          engineer();
+          break;
+        default:
+          intern();
+      }
     });
 }
 
 function engineer() {
-    console.log("an engineer");
+  console.log("an engineer");
 
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Name:",
-            name: "name",
-            default: "Bob" // remove when ready
-        },
-        {
-            type: "input",
-            message: "Employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Github profile:",
-            name: "github"
-        },
+      {
+        type: "input",
+        message: "Name:",
+        name: "name",
+        default: "Maurice Moss",
+      },
+      {
+        type: "input",
+        message: "Employee ID:",
+        name: "id",
+        default: 3,
+      },
+      {
+        type: "input",
+        message: "Email address:",
+        name: "email",
+        default: "m.m@reynholm.com",
+      },
+      {
+        type: "input",
+        message: "Github profile:",
+        name: "github",
+        default: "ivejustfinishedmymilk",
+      },
     ])
     .then(function (userInput) {
-        let engineer = new Engineer(userInput.name, userInput.id, userInput.email, userInput.github);
-        employees.push(engineer)
-        console.log(employees);
-        anotherOne()
+      let engineer = new Engineer(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.github
+      );
+      employees.push(engineer);
+      console.log(employees);
+      anotherOne();
     });
 }
 
 function intern() {
-    console.log("an intern");
+  console.log("an intern");
 
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Name:",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "School:",
-            name: "school"
-        },
+      {
+        type: "input",
+        message: "Name:",
+        name: "name",
+        default: "Richmond Avenal",
+      },
+      {
+        type: "input",
+        message: "Employee ID:",
+        name: "id",
+        default: "2",
+      },
+      {
+        type: "input",
+        message: "Email address:",
+        name: "email",
+        default: "coff@fromgoth2boss.com",
+      },
+      {
+        type: "input",
+        message: "School:",
+        name: "school",
+        default: "Felicity U",
+      },
     ])
     .then(function (userInput) {
-        let intern = new Intern(userInput.name, userInput.id, userInput.email, userInput.school);
-        employees.push(intern)
-        console.log(employees);
-        anotherOne()
+      let intern = new Intern(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.school
+      );
+      employees.push(intern);
+      console.log(employees);
+      anotherOne();
     });
 }
 
 function anotherOne() {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "list",
-            message: "Do you want to enter another employee?",
-            name: "another",
-            choices: ["yes", "no"],
-            default: "no" // remove when ready
-        },
+      {
+        type: "list",
+        message: "Do you want to enter another employee?",
+        name: "another",
+        choices: ["yes", "no"],
+        default: "yes",
+      },
     ])
     .then(function (userInput) {
-        if (userInput.another == "yes") {
-            team()
-        } else {
-            console.log(employees);
-            console.log("thanks for playing");
-            createHTML(employees);
-        }
-    }); 
+      if (userInput.another == "yes") {
+        team();
+      } else {
+        console.log(employees);
+        console.log("thanks for playing");
+        createHTML(employees);
+      }
+    });
 }
 
 function createHTML() {
+  const filename = `dist/index.html`;
 
-    const filename = `dist/index.html`;
+  fs.writeFile(filename, makeThing(employees), (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+}
 
-    fs.writeFile(filename, makeThing(employees), (err) =>
-        err ? console.log(err) : console.log('Success!')
-    );
-
-};
-
-manager()
+manager();
